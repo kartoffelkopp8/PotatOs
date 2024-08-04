0ee28a2d441e7c72089b7dc81bc410f939b3ce5a
felix <felix.blenk@fau.de>
2024-08-04 13:51:41 +0200

    all

---
+++ Makefile
@@ -0,0 +1,40 @@
+
+# tools
+CC = ~/opt/x86_64-elf-7.5.0-Linux-x86_64/bin/x86_64-elf-gcc 
+LD = ~/opt/x86_64-elf-7.5.0-Linux-x86_64/bin/x86_64-elf-ld
+AS = nasm
+
+# flags
+AS_FLAGS = -f elf64
+LD_SCRIPT = -T scripts/linker.ld
+
+# variables
+SRC_DIR := src/kernel/x86_64
+BUILD_DIR = build/x86_64
+
+asm_x86_64_src := $(wildcard $(SRC_DIR)/*.asm)
+asm_x86_64_obj := $(patsubst $(SRC_DIR)/%.asm, $(BUILD_DIR)/%.o, $(asm_x86_64_src))
+
+# source- and objektfiles
+asm_x86_64_src := $(wildcard $(SRC_DIR)/*.asm)
+asm_x86_64_obj := $(patsubst $(SRC_DIR)/%.asm, $(BUILD_DIR)/%.o, $(asm_x86_64_src))
+
+all: $(asm_x86_64_obj)
+
+
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.asm
+	@mkdir -p $(BUILD_DIR)  
+	$(AS) $(AS_FLAGS) $< -o $@
+
+.Phony: build-x86_64 clean
+
+build-x86_64:
+	$(LD) $(LD_SCRIPT) -n -o build/kernel.bin $(asm_x86_64_obj)
+	cp build/kernel.bin iso/boot/ 
+	grub-mkrescue -o kernelx86_64.iso iso
+
+# Bereinigen
+clean:
+	rm -rf $(BUILD_DIR)
+	rm build/kernel.bin
+	rm iso/boot/kernel.bin
---
+++ src/kernel/x86_64/main.asm
@@ -0,0 +1,61 @@
+[bits 32]
+%define TEXT_RAM_COLOUR 0x04
+%define TEXT_RAM 0x8b000
+
+global _start
+
+ section .text 
+_start:
+  
+  mov esp, STACK_TOP
+  ; check Multiboot
+  
+
+  ; check cpuid
+  pushfd                  ; push eflags
+  pushfd
+  xor DWORD [esp], 0x00200000  ; invert ID BIT in Eflags 
+  popfd                        ; set eflags to possibly inverted id bit
+  pushfd 
+  pop eax                      ; pop (maybe modified) eflags into eax 
+  xor eax, [esp]               ; check for changes 
+  popfd                        ; reset eflags to original value 
+  and eax, 0x00200000          ; check if changed = one 1 bit else all 0 
+  jz NO_CPUID
+
+  ; check if long mode is available
+  mov eax, 0x80000000           ; cpuid function (higest supported function)
+  cpuid 
+  cmp eax, 0x80000001           ; check if long mod test is supported 
+  jb NO_LONG
+
+  mov eax, 0x80000001            ; cpuid function (extended Processor info bit 29 = long mode)
+  cpuid 
+  and edx, 0x20000000            ; check bit
+  jz NO_LONG
+  
+hltBenQ EW3880R
+
+NO_LONG:
+  mov al, 0x32
+  jmp PRINT_ERROR
+
+NO_CPUID:
+  mov al, 0x31                ; error code 1
+  jmp PRINT_ERROR
+
+
+
+; error code saved in al
+PRINT_ERROR:
+  mov dword [0xb8000], 0x04520445
+	mov dword [0xb8004], 0x043a0452
+	mov dword [0xb8008], 0x04200420
+	mov byte  [0xb800a], al
+	hlt
+
+
+SECTION .bss
+STACK_BOTTOM:
+  resb 1024 * 32         ; 32 kib stack
+STACK_TOP:
